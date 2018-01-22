4ca817c024b20a3df004109d4e3d2992471731a2
Yusuke Yamamoto <yusuke@mac.com>
2018-01-22 15:33:35 +0900

    add id to Battle

--- src/main/java/ika4j/Battle.kt
+++ src/main/java/ika4j/Battle.kt
@@ -13,12 +13,14 @@ import org.json.JSONObject
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-data class Battle(val rule: Rule,
+data class Battle(val id:Long,
+                  val rule: Rule,
                   val stageA: Stage,
                   val stageB: Stage,
                   val startTime: LocalDateTime,
                   val endTime: LocalDateTime) {
     constructor(battle: JSONObject) : this(
+            id = battle.getLong("id"),
             rule = Rule(battle.getJSONObject("rule").getString("key"), battle.getJSONObject("rule").getString("multiline_name"), battle.getJSONObject("rule").getString("name")),
             startTime = LocalDateTime.ofEpochSecond(battle.getLong("start_time"), 0, ZoneOffset.of("+9")),
             endTime = LocalDateTime.ofEpochSecond(battle.getLong("end_time"), 0, ZoneOffset.of("+9")),
